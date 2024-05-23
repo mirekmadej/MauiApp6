efc9747803e1b9fd1869dd36910555fe4efa5012
Mirek Madej <m.madej@zsp3zamosc.pl>
2024-05-23 09:01:29 +0200

    Add project files.

---
+++ MauiApp6/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace MauiApp6
+{
+    public partial class MainPage : ContentPage
+    {
+        private static string ml = "abcdefghijklmnopqrstuvwxyz";
+        private static string dl = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private static string cy = "01234567890";
+        private static string zs = "!@#$%^&*()_+-=,.<>?;:";
+        private static string s = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerujClicked(object sender, EventArgs e)
+        {
+            s = "";
+            if (chkMale.IsChecked)
+                s += ml;
+            if(chkDuze.IsChecked)
+                s += dl;
+            if(chkCyfry.IsChecked)
+                s += cy;
+            if(chkZnaki.IsChecked)
+                s += zs;
+            
+
+            lblHaslo.Text = s;
+
+
+        }
+    }
+
+}
